474da505907fc916d55fe58c8b8fbca765535ddd
Quang Pham Mac <phamquang131098@gmail.com>
2026-02-15 22:15:39 +0100

    2.Hochladen

--- confirm_Ending_Tool_Freecodecamp.js
+++ confirm_Ending_Tool_Freecodecamp.js
@@ -1,5 +1,4 @@
-const confirmEnding = (str1,str2) => {
-    return str1.slice(str1.length- str2.length) === str2;
-};
+const confirmEnding = (str1,str2) => str1.slice(str1.length- str2.length) === str2;
+
 
 console.log(confirmEnding("Deutschland","land"))
