2be5310dff000137e68506d04179eb3658585946
quangpt98 <167915532+quangpt98@users.noreply.github.com>
2026-04-07 00:58:15 +0200

    Kommentar hinzufügen
    Added a comment explaining the confirmEnding function.

--- confirm_Ending_Tool_Freecodecamp.js
+++ confirm_Ending_Tool_Freecodecamp.js
@@ -1,4 +1,4 @@
 const confirmEnding = (str1,str2) => str1.slice(str1.length- str2.length) === str2;
 
-
+// Str1.Length = 11 , Str2.length = 4, 11-4 = 7 => str1.slice(7)
 console.log(confirmEnding("Deutschland","land"))
